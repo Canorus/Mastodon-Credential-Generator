4ab4c61f6df92989008c0205c38e247a7644905b
Canorus <helios7157@gmail.com>
2020-08-25 23:10:16 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Mastodon-Credential-Generator
-A Python script that generates authentication key for using Mastodon API
+# Mastodon Credential Generator
+
+마스토돈 API를 활용하기 위한 키를 생성해주는 파이선 스크립트입니다.
+
+A Python script that generates authentication key for using Mastodon API.
+
+# 사용법 Usage
+
+```
+python credential.py [action]
+```
+
+## action
+
+- `register`
+
+새로운 키를 생성합니다.
+
+Generates a new key
+
+- `delete`
+
+등록된 키를 삭제합니다. 삭제 과정 중에 키가 담긴 파일명을 요구할 것입니다.
+
+Deletes previously registered key. May require filename containing credential.
+
+- - -
+
+파이선 스크립트 내에서 `import credential` 하여 `retrieve()` 함수를 사용할 수 있습니다. `사용자명`과 `인스턴스 주소`, `인증 내역이 저장된 파일명`을 요구할 수 있습니다.
+
+You can use `retrieve()` function by `import credential` inside Python script. May require `username`, `instance address` and `filename containing auth credential`.
+
+# Requirements
+
+- Python3
+- [Requests](https://github.com/psf/requests)
---
+++ credential.py
@@ -0,0 +1,119 @@
+import requests
+import json
+import sys
+import os
+
+base = os.path.join(os.path.dirname(os.path.abspath(__file__)),'')
+
+def chk_(url):
+    if url[:5] != 'https':
+        return 'https://' + url
+    else:
+        return url
+
+def per(i):
+    follow = int(i / 4)
+    write = int(i % 4 / 2)
+    read = int(i % 4 % 2 / 1)
+    per = ''
+    if read:
+        per += 'read'
+    if write:
+        per += ' write'
+    if follow:
+        per += ' follow'
+    return per
+
+def register(instance, *args):
+    instance = chk_(instance)
+    client_name = input('Please input your client name: (default: credential_generator) ')
+    if client_name == '':
+        client_name = 'credential_generator'
+    if args:
+        p = per(args[0])
+    else:
+        p = per(1)
+    data = {'client_name': client_name,'redirect_uris': 'urn:ietf:wg:oauth:2.0:oob', 'scopes': p}
+    r = requests.post(instance+'/api/v1/apps', data=data)
+    rdata = r.json()
+    client_id = rdata['client_id']
+    client_secret = rdata['client_secret']
+    import webbrowser
+    p = p.replace(' ','%20')
+    webbrowser.open(instance+'/oauth/authorize?client_id='+client_id +'&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code&scope='+p)
+    code = input('input you code from browser: ')
+    print('your access_code is: '+code)
+    auth_data = {'client_id': client_id, 'client_secret': client_secret, 'code': code,'grant_type': 'authorization_code', 'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob'}
+    rauth = requests.post(instance + '/oauth/token', data=auth_data)
+    access_token = rauth.json()['access_token']
+    username = json.loads(requests.get(instance+'/api/v1/accounts/verify_credentials',headers={'Authorization': 'Bearer '+access_token}).content)['acct']
+    user = {}
+    user[username] = access_token
+    login = {}
+    login[instance] = user
+    file_name = input('Set filename: (default: cred.json)')
+    if not file_name.endswith('.json'):
+        file_name += '.json'
+    elif file_name == '':
+        file_name = 'cred.json'
+    with open(base + file_name, 'w') as fw:
+        json.dump(login, fw)
+    return 0
+
+def retrieve(username, instance, filename):
+    import os
+    instance = chk_(instance)
+    if not filename.enswith('.json'):
+        filename += '.json'
+    with open(base + filename) as f:
+        cred = json.load(f)
+    if instance in cred:
+        if username in cred[instance]:
+            return cred[instance][username]
+        else:
+            return register(instance)
+    else:
+        return register(instance)
+
+def delcred(username, instance):
+    instance = chk_(instance)
+    fn = input('please input filename of credential: ')
+    if not fn.endswith('.json'):
+        fn += '.json'
+    with open(base + fn) as fr:
+        cred = json.load(fr)
+    try:
+        cred[instance].pop(username)
+        if len(cred[instance]) == 0:
+            cred.pop(instance)
+    except:
+        print('matching user credential not found')
+    with open(base + fn, 'w') as f:
+        json.dump(cred,f)
+
+if __name__=='__main__':
+    action = sys.argv[1]
+    if action == 'register':
+        inst = input('please input your instance address: ')
+        sc = input('please input scope in unix permission: (default: 1)')
+        if sc == '':
+            sc = 1
+        else:
+            try:
+                sc = int(sc)
+            except:
+                print('only int is accepted for scope')
+                raise ValueError
+        register(inst, sc)
+    elif action == 'delete':
+        try:
+            username = sys.argv[2]
+            instance = sys.argv[3]
+            delcred(username, instance)
+        except:
+            username = input('please input your username: ')
+            instance = input('please input your instance address: ')
+            delcred(username, instance)
+    else:
+        print('input action')
+        raise ValueError
